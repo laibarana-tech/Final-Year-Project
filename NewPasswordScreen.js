b6f19ffc409f20cb2d9e4b8f1e51ed79da908610
laibarana-tech <106876570+laibarana-tech@users.noreply.github.com>
2025-03-08 16:33:00 -0500

    Update NewPasswordScreen.js

--- NewPasswordScreen.js
+++ NewPasswordScreen.js
@@ -1,126 +1,98 @@
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
 import React from 'react';
-import CustomTextInput from '../../Components/TextInputField';
+import {View, Text, TextInput, StyleSheet, ScrollView} from 'react-native';
+
+import CustomHeader from '../../Components/CustomHeader';
 import {ICONS} from '../../Constants/icons';
 import UniversalButton from '../../Components/universalButton';
 
-const NewPasswordScreen = ({navigation}) => {
+const ForgotPasswordScreen = ({navigation}) => {
   return (
-    <ScrollView style={styles.scrollViewContainer}>
-      <View style={styles.headerContainer}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Text style={styles.backText}>Back</Text>
-        </TouchableOpacity>
+    <ScrollView style={styles.container}>
+      <View>
+        {/* Custom Header with Two SVG Icons */}
+        <CustomHeader
+          title="Forget Password"
+          BackgroundSvg={ICONS.illustrationSadIcon}
+          SadIconSvg={ICONS.sadIcon}
+        />
         <View style={styles.textContainer}>
-          <Text style={styles.titleText}>Create New Password</Text>
-          <Text style={styles.subTitleText}>
-            Enter your registered email below
+          <Text style={styles.title}>Forgot Password</Text>
+          <Text style={styles.subtitle}>Enter your registered email below</Text>
+        </View>
+
+        <View style={styles.inputTextContainer}>
+          <Text style={styles.label}>Email address</Text>
+          <TextInput style={styles.input} placeholder="Enter Email" />
+          <Text style={styles.signInText}>
+            Remember the password?{' '}
+            <Text style={styles.signInLink}>Sign in</Text>
           </Text>
         </View>
-      </View>
-      <View style={styles.textFieldContainer}>
-      <Text style={styles.labelTextField}>New Password</Text>
-   
-      <CustomTextInput/>
-      <Text style={styles.labelTextField}>Confirm Password</Text>
-      <CustomTextInput />
-        </View>
-      <View style={styles.illustrationContainer}>
-        <ICONS.SafeFood />
-
-        <UniversalButton
+        <View style={styles.buttonContainerStyle}>
+          <UniversalButton
             buttonStyle={styles.buttonContainerStyle}
             label="Submit"
             onPress={() => navigation.navigate('NewPasswordScreen')}
           />
-
+        </View>
       </View>
     </ScrollView>
   );
 };
 
-export default NewPasswordScreen;
-
 const styles = StyleSheet.create({
-  scrollViewContainer: {
+  container: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: 30,
-    paddingHorizontal: 18,
-  },
-  headerContainer: {
-    
-    marginTop: 10,
-  },
-  backText: {
-    fontSize: 18,
-    fontWeight: '700',
+    paddingHorizontal: 20,
+    paddingTop: 24,
   },
   textContainer: {
     marginTop: 30,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+    marginBottom: 15,
   },
-  titleText: {
-    fontSize: 20,
+  title: {
+    fontSize: 24,
     fontWeight: '700',
-    color: 'rgba(61, 83, 0, 1)',
-    marginTop: 20,
+    color: 'rgba(39, 56, 38, 1)',
   },
-  subTitleText: {
+  subtitle: {
     fontSize: 16,
-    fontWeight: '500',
     color: 'rgba(156, 163, 175, 1)',
-    lineHeight: 21,
-    marginTop: 8,
+    marginTop: 4,
   },
-  textFieldContainer: {
-    marginTop: 20,
-    paddingVertical: 30,
-    
-  },
-  labelTextField: {
- 
-    fontWeight: 'bold',
-    paddingHorizontal: 5,
-    marginBottom: -20,
-    fontSize: 14,
-    lineHeight: 30,
-    color: 'rgb(3, 4, 5)',
-    marginLeft: 10,
-    
-  },
-  buttonContainerStyle: {
-    marginTop: 50,
-    height: 60,
-    width: '80%',
-    borderRadius: 10,
-    backgroundColor: 'rgb(169, 178, 187)',
-   
-  },
-  illustrationContainer: {
-    
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonContainer: {
-    marginTop: 30,
-  },
-  buttonStyle: {
-    borderRadius: 30,
-  },
-  buttonContainerStyle: {
-    backgroundColor: 'rgb(132, 133, 134)',
-    alignSelf: 'center',
+  inputTextContainer: {
     marginTop: 40,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  input: {
+    width: '98%',
+    height: '25%',
+    borderWidth: 1.5,
+    borderColor: '#000',
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+
+  signInText: {
+    textAlign: 'center',
+    color: 'gray',
+  },
+  signInLink: {
+    color: '#556B2F',
+    fontWeight: 'bold',
+  },
+  buttonContainerStyle: {
+    alignSelf: 'center',
+    marginTop: 50,
     width: '90%',
     height: 60,
     borderRadius: 20,
   },
 });
+
+export default ForgotPasswordScreen;
